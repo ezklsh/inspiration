470642407f5b5a80656ce41a9f5623a84353ef65
ezeksuresh03 <ezeksuresh03@gmail.com>
2026-08-22 22:37:48 +0800

    chore: update description

--- js/data.js
+++ js/data.js
@@ -9,8 +9,7 @@ const POSTS = [
     {
         slug: "the-odyssey",
         title: "the odyssey",
-        description: `All the things you wanted to be and then all the things you wished you never wished for.
-It was a delicious itch you want to scratch but you find it under the skin and can't reach it. So the delicious itch becomes unbearable. It told you what you most want is what you most can't have and that the most you can't have is what you already had and lost. It was a song of all the promises I failed to keep. And it told me I don't really want to go home.`,
+        description: `"All the things you wanted to be and then all the things you wished you never wished for.\n\nIt was a delicious itch you want to scratch but you find it under the skin and can't reach it.\n\nSo the delicious itch becomes unbearable.\n\nIt told you what you most want is what you most can't have and that the most you can't have is what you already had and lost.\n\nIt was a song of all the promises I failed to keep.\n\nAnd it told me I don't really want to go home."`,
         cover: "assets/the-odyssey-1786890922.webp",
         gallery: ["assets/the-odyssey-1786890922.webp"],
         related: []
