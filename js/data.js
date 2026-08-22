bbc31a272a87f2f8c7f5f6282d84fd54f2ea4394
ezeksuresh03 <ezeksuresh03@gmail.com>
2026-08-22 22:22:11 +0800

    chore: updated description for the odyssey post

--- js/data.js
+++ js/data.js
@@ -9,7 +9,8 @@ const POSTS = [
     {
         slug: "the-odyssey",
         title: "the odyssey",
-        description: `test description`,
+        description: `All the things you wanted to be and then all the things you wished you never wished for.
+It was a delicious itch you want to scratch but you find it under the skin and can't reach it. So the delicious itch becomes unbearable. It told you what you most want is what you most can't have and that the most you can't have is what you already had and lost. It was a song of all the promises I failed to keep. And it told me I don't really want to go home.`,
         cover: "assets/the-odyssey-1786890922.webp",
         gallery: ["assets/the-odyssey-1786890922.webp"],
         related: []
